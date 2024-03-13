12cd5864c245667fe36bd7d24b2f9a2015ac22b2
son tran <sontran1139@gmail.com>
2024-03-13 16:33:39 +0700

    update arrowButton

--- Assets/Script/SwipeController.cs
+++ Assets/Script/SwipeController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -20,13 +20,25 @@ public class SwipeController : MonoBehaviour
     [SerializeField] Sprite barClosed, barOpen;
 
     [SerializeField] Button previousBtn, nextBtn;
-
+    Vector3 initialPosition;
     private void Awake()
     {
         currentPage = 1;
         targetPos = levelPageRect.localPosition;
+        initialPosition = levelPageRect.localPosition;
         UpdateBar();
         UpdateArrowButton();
+    }
+
+    Vector3 CalculateTargetPosition(int targetPage)
+    {
+        float newX = initialPosition.x + (pageStep.x * (targetPage - 1));
+        return new Vector3(newX, levelPageRect.localPosition.y, levelPageRect.localPosition.z);
+    }
+
+    private void Start()
+    {
+        levelPageRect.GetComponent<ScrollRect>().enabled = false;
     }
     public void NextBtn()
     {
@@ -70,5 +82,12 @@ public class SwipeController : MonoBehaviour
         previousBtn.interactable = true;
         if (currentPage == 1) previousBtn.interactable = false;
         else if (currentPage == maxPage) nextBtn.interactable = false;
+    }
+
+    public void ArrowButtonClick(int pageIndex)
+    {
+        currentPage = pageIndex + 1;
+        targetPos = CalculateTargetPosition(currentPage);
+        MovePage();
     }
 }
